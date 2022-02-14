0d48473a92c60c173d2f68a2083c1c16275e3ef1
noam <noamsoudai@gmail.com>
2022-02-14 21:28:49 +0200

    hello

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+// import "./Header.css";
+
+// function Header() {
+//   return (
+//     <header className="header-music-div">
+//       <div className="manu-div-navar">
+//         <button className="btn-register-music">Register</button>
+//         <div></div>
+//       </div>
+//     </header>
+//   );
+// }
+
+// export default Header;
---
+++ src/views/Home/Home.js
@@ -0,0 +1,11 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="home-div-first">
+      <div className="top-ar-div">
+        <h1>hello</h1>
+      </div>
+    </div>
+  );
+}
